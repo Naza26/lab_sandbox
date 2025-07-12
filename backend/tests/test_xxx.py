e27aebc2939c353f372487d8b29c2229e79c4773
nazarena_rueda <naza120@gmail.com>
2025-07-12 11:09:24 -0300

    add tests to process multiple inputs

--- backend/tests/test_xxx.py
+++ backend/tests/test_xxx.py
@@ -2,12 +2,12 @@ import unittest
 
 
 class Pipe:
-    def __init__(self, pipeline_input):
-        self._pipeline_input = pipeline_input
+    def __init__(self, *inputs):
+        self._pipeline_inputs = PipeData(list(inputs))
         self._steps = []
 
     def output(self):
-        return self.next_step_input()
+        return self.next_step_input().data()
 
     def step(self, step_name, step_function):
         self._steps.append(Step(step_name, self.next_step_input(), step_function))
@@ -15,7 +15,7 @@ class Pipe:
 
     def next_step_input(self):
         if not self._steps:
-            return self._pipeline_input
+            return self._pipeline_inputs
         return self._steps[-1].step_output()
 
 
@@ -24,10 +24,10 @@ class Step:
         self._step_name = step_name
         self._step_function = step_function
         self._step_input = step_input
-        self._step_output = PipeData(self._step_function(self._step_input.data()))
+        self._step_outputs = PipeData(self._step_function(step_input.data()))
 
     def step_output(self):
-        return self._step_output
+        return self._step_outputs
 
 
 class PipeData:
@@ -45,40 +45,49 @@ class PipeData:
 
 
 class PipeTestCase(unittest.TestCase):
-    def test_01(self):
+    def test_01_an_empty_pipeline_returns_input_as_output(self):
         # Given
-        filename = ""
-        pipeline_input = PipeData(filename)
+        pipeline_input = 0
 
         # When
         pipe = Pipe(pipeline_input)
 
         # Then
-        self.assertEqual(pipe.output(), pipeline_input)
+        self.assertEqual(pipe.output(), [pipeline_input])
 
-    def test_02(self):
+    def test_02_a_pipeline_can_execute_a_step_successfully(self):
         # Given
-        pipeline_input = PipeData(0)
+        pipeline_input = 0
 
         # When
-        pipeline_output = PipeData(1)
-        pipe = Pipe(pipeline_input).step("my_first_step", lambda x: x + 1)
+        pipe = Pipe(pipeline_input).step("my_first_step", lambda x: [x[0] + 1])
 
         # Then
-        self.assertEqual(pipe.output(), pipeline_output)
+        self.assertEqual(pipe.output(), [1])
 
-    def test_03(self):
+    def test_03_a_pipeline_can_execute_multiple_steps_successfully(self):
         # Given
-        pipeline_input = PipeData(0)
+        pipeline_input = 0
 
         # When
-        pipeline_output = PipeData(2)
         pipe = (Pipe(pipeline_input)
-                .step("my_first_step", lambda x: x + 1)
-                .step("my_second_step", lambda x: x + 1))
+                .step("my_first_step", lambda x: [x[0] + 1])
+                .step("my_second_step", lambda x: [x[0] + 1]))
 
         # Then
-        self.assertEqual(pipe.output(), pipeline_output)
+        self.assertEqual(pipe.output(), [2])
+
+    def test_04_a_pipeline_with_more_than_one_input_executes_successfully(self):
+        # Given
+        one_pipeline_input = 1
+        another_pipeline_input = 1
+
+        # When
+        pipe = (Pipe(one_pipeline_input, another_pipeline_input)
+                .step("my_first_step", lambda x: [x[0] + x[1]]))
+
+        # Then
+        self.assertEqual(pipe.output(), [2])
 
 
 if __name__ == '__main__':
