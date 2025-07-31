301ae0b18cfa0460ab2e86d0f478f7d5ee0cc18e
Agustin Gonzalez <aguselgonza@gmail.com>
2025-07-31 10:54:52 -0300

    subfolders

--- version0/isx_pipeline/isx_pipeline.py
+++ version0/isx_pipeline/isx_pipeline.py
@@ -2,6 +2,9 @@ import os
 from ..ci_pipe.pipeline import CIPipe
 import json
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMP_DIR = os.path.join(BASE_DIR, "temp_output")
+
 class ISXPipeline(CIPipe):
     def __init__(self, isx, output_folder, inputs):
         super().__init__(inputs)
@@ -20,14 +23,58 @@ class ISXPipeline(CIPipe):
         trace_path = os.path.join(output_folder, "trace.json")
         if not os.path.exists(trace_path):
             with open(trace_path, 'w') as f:
-                json.dump({
-                    "steps": [],
-                    "inputs": inputs,
-                    "output": []
-                }, f, indent=4)
-
+                f.write("{}")
         return output_folder, trace_path
     
+    def step(self, step_name, step_function, *args):
+        step_index = len(self._steps)
+        step_folder_name = f"step {step_index + 1} - {step_name}"
+        step_folder_path = os.path.join(self._output_folder, step_folder_name)
+        os.makedirs(step_folder_path, exist_ok=True)
+
+        updated_args = []
+        for arg in args:
+            if isinstance(arg, list) and all(isinstance(x, str) for x in arg):
+                new_paths = [os.path.join(step_folder_path, os.path.basename(p)) for p in arg]
+                updated_args.append(new_paths)
+            else:
+                updated_args.append(arg)
+
+        result = super().step(step_name, step_function, *updated_args)
+
+        step_info = self._steps[-1].info()
+        self._update_trace(step_info)
+
+        return result
+    
+
+
+    def _update_trace(self, step_info):
+        with open(self._trace_file, "r") as f:
+            trace = json.load(f)
+
+        def extract_paths(data):
+            if isinstance(data, dict):
+                for v in data.values():
+                    if isinstance(v, list):
+                        return v
+                return []
+            elif isinstance(data, list):
+                return data
+            else:
+                return []
+
+        step_number = str(len(self._steps))
+
+        trace[step_number] = {
+            "algoritmo": step_info["name"],
+            "input": extract_paths(step_info["input"]),
+            "output": extract_paths(step_info["output"])
+        }
+
+        with open(self._trace_file, "w") as f:
+            json.dump(trace, f, indent=4)
+
     def info(self):
         with open(self._trace_file, "r") as f:
             return json.load(f)
@@ -35,7 +82,6 @@ class ISXPipeline(CIPipe):
 
     @classmethod
     def _scan_files(self , input_folder: str):
-        import os
         # Check existence
         files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith('.isxd')]
         return files
@@ -46,11 +92,10 @@ class ISXPipeline(CIPipe):
 
     def preprocess_videos(self):
         input_files = self.next_step_input()
-
         pp_files = self._isx.make_output_file_paths(input_files, self._output_folder, 'PP')
         return self.step('Preprocess Videos', lambda files, pp: self.preprocess_result(files, pp), pp_files)
 
-    def preprocess_result(self,files, pp):
+    def preprocess_result(self, files, pp):
         self._isx.preprocess(files, pp)
         return { 'videos': pp }
 
