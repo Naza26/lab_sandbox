ec945567504afcfd8287f213e69d8baf5a56a5bc
josinger <josinger@fi.uba.ar>
2025-08-10 17:16:52 -0300

    refactors

--- version0/isx_pipeline/isx_pipeline.py
+++ version0/isx_pipeline/isx_pipeline.py
@@ -10,89 +10,33 @@ class ISXPipeline(CIPipe):
         super().__init__(inputs)
         self._isx = isx
         self._steps = []
-        self._output_folder, self._trace_file = self.create_output_folder(output_folder, inputs)
+        self._output_folder, self._trace_file = self._create_output_folder(output_folder)
 
     @classmethod
     def new(cls, isx, input_folder, output_folder="output"):
         inputs = cls._scan_files(input_folder)
         return cls(isx, output_folder, inputs)
-
-    def create_output_folder(self, output_folder, inputs):
-        os.makedirs(output_folder, exist_ok=True)
-
-        trace_path = os.path.join(output_folder, "trace.json")
-        if not os.path.exists(trace_path):
-            with open(trace_path, 'w') as f:
-                f.write("{}")
-        return output_folder, trace_path
+    
+    @classmethod
+    def _scan_files(self , input_folder: str):
+        files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith('.isxd')]
+        return { "videos": files }
     
     def step(self, step_name, step_function, *args):
         step_folder_path = self._step_folder_path(step_name)
         os.makedirs(step_folder_path, exist_ok=True)
 
-        updated_args = []
-        for arg in args:
-            if isinstance(arg, list) and all(isinstance(x, str) for x in arg):
-                new_paths = [os.path.join(step_folder_path, os.path.basename(p)) for p in arg]
-                updated_args.append(new_paths)
-            else:
-                updated_args.append(arg)
-
-        result = super().step(step_name, step_function, *updated_args)
-
-        step_info = self._steps[-1].info()
-        self._update_trace(step_info)
+        result = super().step(step_name, step_function, *args)
+        self._update_trace()
 
         return result
-    
-    def _step_folder_path(self, step_name):
-        step_index = len(self._steps)
-        step_folder_name = f"step {step_index + 1} - {step_name}"
-        return os.path.join(self._output_folder, step_folder_name)
-
-    def _update_trace(self, step_info):
-        with open(self._trace_file, "r") as f:
-            trace = json.load(f)
-
-        def extract_paths(data):
-            if isinstance(data, dict):
-                for v in data.values():
-                    if isinstance(v, list):
-                        return v
-                return []
-            elif isinstance(data, list):
-                return data
-            else:
-                return []
-
-        step_number = str(len(self._steps))
-
-        trace[step_number] = {
-            "algorithm": step_info["name"],
-            "input": extract_paths(step_info["input"]),
-            "output": extract_paths(step_info["output"])
-        }
-
-        with open(self._trace_file, "w") as f:
-            json.dump(trace, f, indent=4)
 
     def trace(self):
         with open(self._trace_file, "r") as f:
             return json.load(f)
 
-
-    @classmethod
-    def _scan_files(self , input_folder: str):
-        # Check existence
-        files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith('.isxd')]
-        return files
-
-        # "preprocess_videos",
-        # "bandpass_filter_videos",
-        # "motion_correction_videos",
-
     def preprocess_videos(self, name="Preprocess Videos"):
-        input_files = self.next_step_input()
+        input_files = self.next_step_input()['videos']
         pp_files = self._isx.make_output_file_paths(input_files, self._step_folder_path(name), 'PP')
         return self.step(name, lambda files, pp: self._preprocess_result(files, pp), pp_files)
 
@@ -125,8 +69,39 @@ class ISXPipeline(CIPipe):
         event_files = self._isx.make_output_file_paths(input_files, self._step_folder_path(name), 'ED')
         return self.step(name, lambda files, events: self._detect_events_in_cells_result(files, events), event_files)
 
-    def _preprocess_result(self, files, pp):
-        self._isx.preprocess(files, pp)
+    def _create_output_folder(self, output_folder):
+        os.makedirs(output_folder, exist_ok=True)
+
+        trace_path = os.path.join(output_folder, "trace.json")
+        if not os.path.exists(trace_path):
+            with open(trace_path, 'w') as f:
+                f.write("{}")
+        return output_folder, trace_path
+
+    def _step_folder_path(self, step_name):
+        step_index = len(self._steps)
+        step_folder_name = f"step {step_index + 1} - {step_name}"
+        return os.path.join(self._output_folder, step_folder_name)
+
+    def _update_trace(self):
+        step_info = self._steps[-1].info()
+        with open(self._trace_file, "r") as f:
+            trace = json.load(f)
+
+        self._add_step_to_trace(step_info, trace)
+        with open(self._trace_file, "w") as f:
+            json.dump(trace, f, indent=4)
+
+    def _add_step_to_trace(self, step_info, trace):
+        step_number = str(len(self._steps))
+        trace[step_number] = {
+            "algorithm": step_info["name"],
+            "input": [item for v in step_info["input"].values() for item in v],
+            "output": [item for v in step_info["output"].values() for item in v]
+        }
+
+    def _preprocess_videos_result(self, files, pp):
+        self._isx.preprocess(files['videos'], pp)
         return { 'videos': pp }
     
     def _bandpass_filter_result(self, files, bp):
@@ -137,8 +112,8 @@ class ISXPipeline(CIPipe):
         self._isx.project_movie(files['videos'], mean_proj, stat_type='mean')
         self._isx.motion_correct(files['videos'], mc, max_translation=20, reference_file_name=mean_proj,
                                   output_translation_files=trans, output_crop_rect_file=crop)
-        return { 'videos': mc, 'translations': trans, 'crop_rect': crop, 'mean_projection': mean_proj }
-    
+        return { 'videos': mc, 'translations': trans, 'crop_rect': [crop], 'mean_projection': [mean_proj] }
+
     def _normalize_dff_result(self, files, dff):
         self._isx.dff(files['videos'], dff, f0_type='mean')
         return { 'videos': dff }
